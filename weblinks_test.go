7712fe47646cfa3e93fc48b4d021d2b2252f482e
Shamsher <ansarisam47@gmail.com>
2016-02-18 09:21:59 +0530

    Updated test case

--- weblinks_test.go
+++ weblinks_test.go
@@ -1,7 +1,21 @@
 package weblinks
 
 import "testing"
+import "strings"
 
 func TestGet(t *testing.T) {
+
+	reader := strings.NewReader(`<p>
+    This webpage contains lot of links
+    <a href="http://google.com"></a>
+    <a href="http://github.com/shamsher31"></a>
+    <a href="http://about.me/shamsher"></a>
+  </p>`)
+
+	links := Get(reader)
+
+	if len(links) != 3 {
+		t.Error("Incorrect number of URL returned")
+	}
 
 }
