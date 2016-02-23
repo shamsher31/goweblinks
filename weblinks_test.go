ff3e3919393650475395971b9eab5dc25f5ad518
Shamsher <ansarisam47@gmail.com>
2016-02-23 22:53:26 +0530

    Add benchmark test

--- weblinks_test.go
+++ weblinks_test.go
@@ -1,16 +1,19 @@
 package weblinks
 
-import "testing"
-import "strings"
+import (
+	"strings"
+	"testing"
+)
 
+var reader = strings.NewReader(`<p>
+  This webpage contains lot of links
+  <a href="http://google.com"></a>
+  <a href="http://github.com/shamsher31"></a>
+  <a href="http://about.me/shamsher"></a>
+</p>`)
+
+// go test -v
 func TestGet(t *testing.T) {
-
-	reader := strings.NewReader(`<p>
-    This webpage contains lot of links
-    <a href="http://google.com"></a>
-    <a href="http://github.com/shamsher31"></a>
-    <a href="http://about.me/shamsher"></a>
-  </p>`)
 
 	links := Get(reader)
 
@@ -18,4 +21,11 @@ func TestGet(t *testing.T) {
 		t.Error("Incorrect number of URL returned")
 	}
 
+}
+
+// go test -test.bench=".*"
+func BenchmarkGet(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Get(reader)
+	}
 }
